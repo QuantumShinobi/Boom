707a1e82302330c6e03bc3551899d60ac0b57dbf
Rishit <63300310+IamEinstein@users.noreply.github.com>
2021-06-28 07:10:19 +0000

    Update code from replit

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-
+from keep_alive import keep_alive
 from discord.ext import commands
 import discord
-from mongo import connect_mongo
 from dotenv import load_dotenv
 import os
-
+from discord.ext.commands import has_permissions
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 client = commands.Bot(command_prefix="b!",
-                      activity=discord.Game("Developed by IamEinstein"), intents=discord.Intents.all())
-collection = connect_mongo()
+                      activity=discord.Game("Developed by IamEinstein(Rishit)"), intents=discord.Intents.all())
+
 
 
 # @client.command()
 # async def say(ctx, what):
 #     user = client.get_user(int(os.getenv("MY_ID")))
 #     await user.send(what)
+
+# @client.command(pass_context=True)
+# @has_permissions(administrator=True)
+# async def disable_log(ctx):
+#   await client.send_message("Okay, stopping del msgs")
+
 
 
 @client.event
@@ -35,12 +40,12 @@ async def on_ready():
 @client.event
 async def on_message_edit(before, after):
     channel = client.get_channel(858296114415534100)
-    await channel.send(f"{before.author.mention} edited a message from\n `{before.content}` to `{after.content}`")
+    await channel.send(f"{before.author} edited a message from\n `{before.content}` to `{after.content}` in {before.channel.mention}")
 
 
 @client.event
 async def on_message_delete(message):
     channel = client.get_channel(858296114415534100)
-    await channel.send(f"{message.author.mention} you deleted a message:\n  {message.content}")
-
+    await channel.send(f"{message.author}  deleted a message in {message.channel.mention}:\n  {message.content}")
+keep_alive()
 client.run(token)

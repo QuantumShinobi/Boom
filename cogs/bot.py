202477b9ed3188881d913162b51c1dd7d26279fd
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-06-28 14:47:15 +0530

    Add cogs, remove gif bug and other stuff.

---
+++ cogs/bot.py
@@ -0,0 +1,63 @@
+import discord
+import datetime
+from discord import Color
+from discord.ext import commands
+
+
+class Bot(commands.Cog):
+    """
+    Basic bot functions 
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        "Tells mods when the bot is ready"
+        channel = self.bot.get_channel(858296114415534100)
+        await channel.send(f"Bot is ready")
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        "Identifies and reports edited messages"
+
+        channel = self.bot.get_channel(858296114415534100)
+        embed = discord.Embed(
+            color=Color.blue(), url="https://github.com/IamEinstein/Boom", title=f"{before.author} edited a message", timestamp=datetime.datetime.now())
+        embed.set_thumbnail(url=before.author.avatar_url)
+        print(after)
+        embed.add_field(name="Original Message",
+                        value=f'{before.content}', inline=False)
+        embed.add_field(name="Edited Message",
+                        value=f'{after.content}', inline=True)
+        if int(before.channel.id) == 858296114415534100 or before.author.bot == True or before.content == after.content:
+            pass
+        else:
+            await channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        channel = self.bot.get_channel(858296114415534100)
+        if int(message.channel.id) == 858296114415534100:
+            pass
+        else:
+            embed = discord.Embed(
+                color=Color.blue(), url="https://github.com/IamEinstein/Boom", title=f"{message.author} deleted a message", timestamp=datetime.datetime.now())
+            embed.set_thumbnail(url=message.author.avatar_url)
+
+            embed.add_field(name="Message Content",
+                            value=f'{message.content}', inline=False)
+            await channel.send(embed=embed)
+
+    @commands.command()
+    async def help(self, ctx, *args, **kwargs):
+        embed = discord.Embed(color=Color.blue(), url="https://github.com/IamEinstein/Boom",
+                              title=f"Boom Bot help,\n here are the list of commands", timestamp=datetime.datetime.now())
+        embed.add_field(name="b!register",
+                        value="Type b!register if you want to get registered as an RMTian ", inline=True)
+        await ctx.send(embed=embed)
+
+
+# keep_alive()
+# client.run(token)
--- main.py
+++ main.py
@@ -1,51 +1,12 @@
-from keep_alive import keep_alive
-from discord.ext import commands
 import discord
-from dotenv import load_dotenv
+from discord.ext import commands
+from cogs.bot import Bot
 import os
-from discord.ext.commands import has_permissions
+from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 client = commands.Bot(command_prefix="b!",
-                      activity=discord.Game("Developed by IamEinstein(Rishit)"), intents=discord.Intents.all())
-
-
-
-# @client.command()
-# async def say(ctx, what):
-#     user = client.get_user(int(os.getenv("MY_ID")))
-#     await user.send(what)
-
-# @client.command(pass_context=True)
-# @has_permissions(administrator=True)
-# async def disable_log(ctx):
-#   await client.send_message("Okay, stopping del msgs")
-
-
-
-@client.event
-async def on_message(message):
-    args = message.content.split()
-    if "help" in args:
-        await message.channel.send(f"Please type `{client.command_prefix}help` for more information")
-    await client.process_commands(message)
-
-
-@client.event
-async def on_ready():
-    channel = client.get_channel(858296114415534100)
-    await channel.send(f"Bot is ready")
-
-
-@client.event
-async def on_message_edit(before, after):
-    channel = client.get_channel(858296114415534100)
-    await channel.send(f"{before.author} edited a message from\n `{before.content}` to `{after.content}` in {before.channel.mention}")
-
-
-@client.event
-async def on_message_delete(message):
-    channel = client.get_channel(858296114415534100)
-    await channel.send(f"{message.author}  deleted a message in {message.channel.mention}:\n  {message.content}")
-keep_alive()
+                      activity=discord.Game("Developed by IamEinstein(Rishit)"), intents=discord.Intents.all(), help_command=None)
+client.add_cog(Bot(client))
 client.run(token)
+

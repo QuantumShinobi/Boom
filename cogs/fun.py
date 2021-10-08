b0fc3acaebf432ede39c3ee11deeb408bf7f7833
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-10-08 22:27:43 +0530

    Remove joke command error

--- cogs/fun.py
+++ cogs/fun.py
@@ -23,14 +23,31 @@ class Fun(commands.Cog):
 
     @commands.command(description=joke_description)
     async def joke(self, ctx: commands.Context):
-        joke_url = "https://official-joke-api.appspot.com/jokes/general/random"
+        # Old api fo`r jokes https://official-joke-api.appspot.com/jokes/general/random
+        # New API for jokes https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw
+        joke_url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw"
         response = requests.get(joke_url)
         json = response.json()
-        title = json[0]['setup']
-        description = json[0]['punchline']
-        embed = discord.Embed(color=give_random_color(),
-                              title=title, description=description)
-        await ctx.message.reply(embed=embed)
+        print(json)
+        type = json['type']
+        if type == "single":
+            joke = json['joke']
+            category = json['category']
+            embed = discord.Embed(color=give_random_color(),
+                                  title="Here's a joke for ya!", description=joke)
+            # embed.add_field(name="category", value=category)
+            await ctx.message.reply(embed=embed)
+        else:
+            setup = json['setup']
+            delivery = json['delivery']
+            category = json['category']
+
+            embed = discord.Embed(color=give_random_color(),
+                                  title="Here's a joke for ya!")
+            embed.add_field(name="Question", value=setup, inline=False)
+            embed.add_field(name="Answer", value=delivery, inline=False)
+            # embed.add_field(name="Category", value=category)
+            await ctx.message.reply(embed=embed)
 
 
 def setup(bot: commands.Bot):
